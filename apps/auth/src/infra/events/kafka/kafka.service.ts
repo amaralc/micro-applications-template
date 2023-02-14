5f12b37afb5652b130bb797a42645c36a5a81495
amaralc <amaral.calil@gmail.com>
2023-02-13 21:30:29 -0300

    config: adjust kafka service

--- apps/auth/src/infra/events/kafka/kafka.service.ts
+++ apps/auth/src/infra/events/kafka/kafka.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Kafka, logLevel } from 'kafkajs';
 
 @Injectable()
-export class KafkaService {
-  private readonly kafka = new Kafka({
-    clientId: 'my-app',
-    brokers: ['kafka:9092'], // replace 'kafka:9092' with your kafka host and port
-    logLevel: logLevel.NOTHING,
-  });
+export class KafkaService implements OnModuleInit {
+  private kafka: Kafka;
 
   async createProducer() {
     const producer = this.kafka.producer();
     await producer.connect();
     return producer;
+  }
+
+  async onModuleInit() {
+    // Connect with database
+    this.kafka = new Kafka({
+      clientId: 'auth',
+      brokers: ['kafka:9092'], // replace 'kafka:9092' with your kafka host and port
+      logLevel: logLevel.NOTHING,
+    });
   }
 }
