dfe22dfcfe09e1c913d8bbbf9b406437e4323399
amaralc <amaral.calil@gmail.com>
2023-02-26 12:56:57 -0300

    config: implement kafka custom transport

--- apps/auth-consumer-only/src/main.ts
+++ apps/auth-consumer-only/src/main.ts
@@ -18,6 +18,28 @@ async function bootstrap() {
         },
       },
     }
+    // {
+    //   /**
+    //    * Custom transporters
+    //    *
+    //    * Docs: @see https://docs.nestjs.com/microservices/custom-transport
+    //    * Articles:
+    //    *  - @see https://dev.to/nestjs/integrate-nestjs-with-external-services-using-microservice-transporters-part-1-p3
+    //    *  - @see https://dev.to/johnbiundo/series/4724
+    //    *  - @see https://dev.to/nestjs/part-1-introduction-and-setup-1a2l
+    //    * Implementation: @see https://github.com/nestjs/nest/issues/3083#issuecomment-1077456005
+    //    */
+
+    //   strategy: new KafkaCustomTransport({
+    //     client: {
+    //       clientId: 'plan-subscription', // plan-subscription-server
+    //       brokers: ['localhost:9092'],
+    //     },
+    //     consumer: {
+    //       groupId: 'plan-subscription',
+    //     },
+    //   }),
+    // }
   );
   await app.listen();
   Logger.log(`🚀 Application is running ...`);
